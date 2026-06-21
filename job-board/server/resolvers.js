7ebcde85ba25fca40bf403c44bf21f4be9da7cbc
Nhat Tran <kevintran11082002@agmail.com>
2026-06-21 19:56:48 -0400

    Integrated apollo server for graphQL

---
+++ job-board/server/resolvers.js
@@ -0,0 +1,5 @@
+export const mockResolvers = {
+  Query:{
+    greeting: () => "hello there friend",
+  }
+}
--- job-board/server/server.js
+++ job-board/server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware as apolloMiddleware } from '@as-integrations/express4';
+import {readFile} from 'node:fs/promises'
+import { mockResolvers } from './resolvers.js';
 
 const PORT = 9000;
 
@@ -11,11 +13,14 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+const schema = await readFile('./schema.graphql','utf8');
+
 //configure apolloserver for graphql API
-const apolloServer = new ApolloServer({ /* need typeDefs and resolvers in here */});
+const apolloServer = new ApolloServer({ typeDefs: schema, resolvers: mockResolvers});
 await apolloServer.start();
 app.use('/graphql', apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port: http://localhost:${PORT}`);
+  console.log(`GraphQL server is running: http://localhost:${PORT}/graphql`);
 });
