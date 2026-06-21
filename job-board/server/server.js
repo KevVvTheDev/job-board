5cad1df8b8c365c4d6976da21bf38d082e985c9f
Nhat Tran <kevintran11082002@agmail.com>
2026-06-21 19:39:24 -0400

    added apolloserver package and initialized it

--- job-board/server/server.js
+++ job-board/server/server.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@as-integrations/express4';
 
 const PORT = 9000;
 
@@ -8,6 +10,11 @@ const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
+
+//configure apolloserver for graphql API
+const apolloServer = new ApolloServer({ /* need typeDefs and resolvers in here */});
+await apolloServer.start();
+app.use('/graphql', apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
