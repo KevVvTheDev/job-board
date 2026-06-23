c9d7434ff8130e78d68759fff3ffab9b7dfcb702
Nhat Tran <kevintran11082002@agmail.com>
2026-06-22 22:21:28 -0400

    added custom obj to graphQL schema

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,5 +1,11 @@
 export const mockResolvers = {
   Query:{
-    greeting: () => "hello there friend",
+    greeting: () => "hello there friend, welcome to the job board!",
+    job: () => ({
+      title: "Software Engineer",
+      name: "Real_company",
+      description: "This is da cool job",
+      id: "real-id-1",
+    })
   }
 }
